4aac1556297466c6286920ecacf7a7eced9e17ae
Oleksandr Hiliazov <oleksandr.hiliazov@gmail.com>
2021-11-17 19:49:03 +0200

    Add tournaments endpoints

--- app/routers/tournament.py
+++ app/routers/tournament.py
@@ -1,12 +1,19 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session
 
-from ..crud import create_instance
+from ..crud import create_instance, delete_instance, get_instances, update_instance
 from ..dependencies import get_session
 from ..models.tournament import Tournament, TournamentCreate, TournamentRead
 
 
-router = APIRouter(tags=["players"])
+router = APIRouter(tags=["tournaments"])
+
+
+@router.get("/tournaments", response_model=List[TournamentRead])
+async def read_tournaments(session: Session = Depends(get_session)):
+    return get_instances(session, Tournament)
 
 
 @router.post("/tournaments", response_model=TournamentRead)
@@ -16,3 +23,30 @@ def create_tournament(
 ):
     tournament = create_instance(session, Tournament.from_orm(data))
     return tournament
+
+
+@router.get("/tournaments/{tournament_id}", response_model=TournamentRead)
+async def read_tournament(tournament_id: int, session: Session = Depends(get_session)):
+    if tournament := session.get(Tournament, tournament_id):
+        return tournament
+    raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+
+@router.put("/tournaments/{tournament_id}", response_model=TournamentRead)
+async def update_tournament(
+    tournament_id: int,
+    data: TournamentCreate,
+    session: Session = Depends(get_session),
+):
+    if tournament := update_instance(session, Tournament, tournament_id, data):
+        return tournament
+    raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+
+@router.delete("/tournaments/{tournament_id}", response_model=TournamentRead)
+async def delete_tournament(
+    tournament_id: int, session: Session = Depends(get_session)
+):
+    if tournament := delete_instance(session, Tournament, tournament_id):
+        return tournament
+    raise HTTPException(status.HTTP_404_NOT_FOUND)
--- utils/database.py
+++ utils/database.py
@@ -1,34 +1,15 @@
 import argparse
-import random
 from typing import List
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, create_session
+from sqlmodel import Session, SQLModel, create_engine
 
-from app.database import Base
-from app.models import Participant, Player, Tournament, User
+from app.models import Game, Participant, Player, Tournament  # noqa: F401
 from app.settings import settings
-from utils.mock_data import (
-    participant_create_data,
-    player_create_data,
-    tournament_create_data,
-    user_create_data,
-)
-
-
-def create_users(db: Session, amount: int = 100) -> List[User]:
-    users = [User(**user_create_data().dict()) for _ in range(amount)]
-    db.add_all(users)
-    db.commit()
-
-    for user in users:
-        db.refresh(user)
-
-    return users
+from tests.helpers import player_create_data
 
 
 def create_players(db: Session, amount: int = 100) -> List[Player]:
-    players = [Player(**player_create_data().dict()) for _ in range(amount)]
+    players = [Player.from_orm(player_create_data()) for _ in range(amount)]
     db.add_all(players)
     db.commit()
 
@@ -36,37 +17,6 @@ def create_players(db: Session, amount: int = 100) -> List[Player]:
         db.refresh(player)
 
     return players
-
-
-def create_tournaments(db: Session, amount: int = 30) -> List[Tournament]:
-    tournaments = [Tournament(**tournament_create_data().dict()) for _ in range(amount)]
-    db.add_all(tournaments)
-    db.commit()
-
-    for tournament in tournaments:
-        db.refresh(tournament)
-
-    return tournaments
-
-
-def create_participants(
-    db: Session,
-    players: List[Player],
-    tournaments: List[Tournament],
-    amount: int = 16,
-) -> List[Participant]:
-    participants = [
-        Participant(**participant_create_data(player.id, tournament.id).dict())
-        for player in random.sample(players, amount)
-        for tournament in tournaments
-    ]
-    db.add_all(participants)
-    db.commit()
-
-    for participant in participants:
-        db.refresh(participant)
-
-    return participants
 
 
 if __name__ == "__main__":
@@ -77,17 +27,7 @@ if __name__ == "__main__":
         help="Database URL",
         default=settings.database_url,
     )
-    parser.add_argument("--users", type=int, default=10, help="Number of users")
     parser.add_argument("--players", type=int, default=100, help="Number of players")
-    parser.add_argument(
-        "--tournaments", type=int, default=30, help="Number of tournaments"
-    )
-    parser.add_argument(
-        "--participants",
-        type=int,
-        default=16,
-        help="Number of participants per tournament",
-    )
     args = parser.parse_args()
 
     print(f"Target database URL: {args.database_url}")
@@ -98,22 +38,15 @@ if __name__ == "__main__":
         exit()
 
     engine = create_engine(args.database_url)
-    session = create_session(
-        engine, autoflush=True, future=True, expire_on_commit=False
-    )
 
     print("Dropping database...", end="")
-    Base.metadata.drop_all(bind=engine)
-    print("OK\nCreating database...", end="")
-    Base.metadata.create_all(bind=engine)
-    print("OK\nCreating users...", end="")
-    db_users = create_users(session, args.users)
-    print("OK\nCreating players...", end="")
-    db_players = create_players(session, args.players)
-    print("OK\nCreating tournaments...", end="")
-    db_tournaments = create_tournaments(session, args.tournaments)
-    print("OK\nCreating participants...", end="")
-    participants = create_participants(
-        session, db_players, db_tournaments, args.participants
-    )
+    SQLModel.metadata.drop_all(bind=engine)
     print("OK")
+    print("Creating database...", end="")
+    SQLModel.metadata.create_all(bind=engine)
+    print("OK")
+
+    with Session(engine) as session:
+        print("Creating players...", end="")
+        db_players = create_players(session, args.players)
+        print("OK")
