32b42fb8107277f627d7a7cfe44b2e643102f383
Oleksandr Hiliazov <oleksandr.hiliazov@gmail.com>
2021-11-17 20:22:57 +0200

    Updated tournament docs

--- app/models/tournament.py
+++ app/models/tournament.py
@@ -25,8 +25,37 @@ class TournamentCreate(TournamentBase):
     mm_dense: bool = True
     handicap_bar: int = -30
     handicap_max: int = 9
-    handicap_correction: int = 0
+    handicap_correction: int = -2
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Championship of Ukraine 2021 - quarterfinals",
+                "number_of_rounds": 5,
+                "mm_floor": -20,
+                "mm_bar": 8,
+                "mm_dense": True,
+                "handicap_bar": -30,
+                "handicap_max": 9,
+                "handicap_correction": -2,
+            }
+        }
 
 
 class TournamentRead(TournamentBase):
     id: int
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "Championship of Ukraine 2021 - quarterfinals",
+                "number_of_rounds": 5,
+                "mm_floor": -20,
+                "mm_bar": 8,
+                "mm_dense": True,
+                "handicap_bar": -30,
+                "handicap_max": 9,
+                "handicap_correction": -2,
+            }
+        }
