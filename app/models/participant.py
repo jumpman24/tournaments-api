30a53a05618501c6654ae2cf9ac361f6006177ea
Oleksandr Hiliazov <oleksandr.hiliazov@gmail.com>
2021-11-18 11:46:05 +0200

    Updated docs

--- app/models/participant.py
+++ app/models/participant.py
@@ -5,19 +5,30 @@ from sqlmodel import Field, SQLModel
 
 class Participant(SQLModel, table=True):
     id: Optional[int] = Field(nullable=False, primary_key=True)
-    player_id: int = Field(foreign_key="player.id")
     tournament_id: int = Field(foreign_key="tournament.id")
+    player_id: int = Field(foreign_key="player.id")
     rating: int
     start_mms: int
     is_final: bool
 
 
 class ParticipantCreate(SQLModel):
-    player_id: int
     tournament_id: int
+    player_id: int
     rating: int
     start_mms: int
     is_final: bool
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "player_id": 1,
+                "tournament_id": 1,
+                "rating": 2100,
+                "start_mms": 15,
+                "is_final": False,
+            }
+        }
 
 
 class ParticipantUpdate(SQLModel):
@@ -25,11 +36,31 @@ class ParticipantUpdate(SQLModel):
     start_mms: int
     is_final: bool
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "rating": 2100,
+                "start_mms": 15,
+                "is_final": False,
+            }
+        }
+
 
 class ParticipantRead(SQLModel):
     id: int
-    player_id: int
     tournament_id: int
+    player_id: int
     rating: int
     start_mms: int
     is_final: bool
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "player_id": 1,
+                "tournament_id": 1,
+                "rating": 2100,
+                "start_mms": 15,
+                "is_final": False,
+            }
+        }
